6bf080df21441c40c4ace8934741a55187f6c4e8
Sergejs Umnovs <sergio30032001@gmail.com>
2023-12-19 20:49:52 +0200

    Added a script for encryption

--- matlab/main.m
+++ matlab/main.m
@@ -1,12 +1,15 @@
 clear variables
-% setup
-img = imread('../res/house.tif');
+%% Image loading
+img = imread('../res/cameraman_50x50.tif');
+img = img(:,:,1);
 img_w = width(img);
+img_h = height(img);
+N_pix = img_w*img_h;
 
+%% cipher
 min_time = 0;
-max_time = 2*img_w;
-x01 = [0; 0; 0];
-x02 = [10; 10; 0];
+max_time = 50; %2*img_w;
+x0 = [0; 0; 0];
 ode_options = odeset('RelTol', 1.0e-6, 'AbsTol', 1.0e-6, 'MaxStep', 1e-3);
 
 N = 5; % approximating line count
@@ -14,40 +17,46 @@ N = 5; % approximating line count
 % fig1 = figure(1); % phase portrait
 % set(fig1,'Position',[0 0 1280 720]);
 
-for k = 1:1
-    
-    disp("Computing first...")
-    [t, var1] = ode45(@(t, var) calc_derivatives(t, var, N), [min_time, max_time], x01, ode_options);
-    x01 = [var1(length(var1(:,1)),1), var1(length(var1(:,2)),2), var1(length(var1(:,3)),3)];
-    disp("Computing second...")
-    [t, var2] = ode45(@(t, var) calc_derivatives(t, var, N), [min_time, max_time], x02, ode_options);
-    x02 = [var2(length(var2(:,1)),1), var2(length(var2(:,2)),2), var2(length(var2(:,3)),3)];
-    
-    disp("Comparing")
-    compare_x = downsample(var1((1:4000*img_w),1) > var2((1:4000*img_w),1), 1000);
-    
-    disp(['Progress: ' num2str(k/img_w*100) '%'])
+%values = uint8(zeros(1, N_vals));
+
+img_encr = uint8(zeros(img_h, img_w));
+tic
+for i = 1:img_h
+    for j = 1:img_w
+        [t, var] = ode45(@(t, var) calc_derivatives(t, var, N), [min_time, max_time], x0, ode_options);
+        x0 = [var(length(var(:,1)),1), var(length(var(:,2)),2), var(length(var(:,3)),3)];
+
+        y = var(:,2);
+        x = var(:,1);
+
+        % get y = 0 line crossings
+        y_cross_indices = find(diff(sign(y)) < 0);
+        y_cross_indices = y_cross_indices(2:end);
+
+        bits = x(y_cross_indices) < 60;
+        if length(bits) >= 8
+            bits = 1*(bits(1:8))';
+        else
+            bits = 1*[bits' zeros(1, 8-length(bits))];
+        end
+        
+        img_bits = de2bi(img(i, j),8);
+        img_encr(i, j) = bi2de(xor(bits, img_bits));
+
+        disp(['Progress = ' num2str(((i-1)*img_w + j-1)/N_pix*100)]);
+    end
 end
+toc
+y_cross = y(y_cross_indices);
+x_cross = x(y_cross_indices);
+plot(x, y); hold on;
+scatter(x_cross, y_cross);
 
+figure(2);
+imshow(img);
 
+%% encrypted
+figure(3);
+imshow(img_encr);
 
-%extract_comparison
-
-
-% x_raw = int32(csvread('../components/vilnius_oscillator/sim/test_out_x.csv'));
-% y_raw = int32(csvread('../components/vilnius_oscillator/sim/test_out_y.csv'));
-% z_raw = int32(csvread('../components/vilnius_oscillator/sim/test_out_z.csv'));
-% 
-% x_int = bitshift(x_raw, -16);
-% y_int = bitshift(y_raw, -16);
-% z_int = bitshift(z_raw, -16);
-% 
-% x_frac = bitand(x_raw, 65535);
-% y_frac = bitand(y_raw, 65535);
-% z_frac = bitand(z_raw, 65535);
-% 
-% x_fpga = double(x_int) + double(x_frac) .* 2^(-16);
-% y_fpga = double(y_int) + double(y_frac) .* 2^(-16);
-% z_fpga = double(z_int) + double(z_frac) .* 2^(-16);
-% 
-% plot3(x_fpga, y_fpga, z_fpga);
+imwrite(img_encr, '../res/cameraman_50x50_encr.tif', 'TIFF');
