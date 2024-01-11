99e374b8998c840b830c6e11dbff4c1c61d51846
Sergejs Umnovs <sergio30032001@gmail.com>
2024-01-11 18:52:28 +0200

    Renamed drawing script for compatibility

--- matlab/draw_comparison.m
+++ matlab/draw_comparison.m
@@ -4,7 +4,7 @@ max_time = 200;
 x0 = [0; 0; 0];
 ode_options = odeset('RelTol', 1.0e-6, 'AbsTol', 1.0e-6, 'MaxStep', 1e-3);
 
-N = 3;
+N = 5;
 
 C1 = 1e-9; % 1e-9 
 C2 = 150e-12; % 150e-12
@@ -23,10 +23,10 @@ for n = N:N
     %toc;
     x = var(:,1);
     y = var(:,2);
-    z = var(:,3);
+    %z = var(:,3);
     figure(1);
     hold on
-    plot3(x, y, z);
+    plot(x, y);
     
 end
 
@@ -38,18 +38,21 @@ ylabel('y');
 
 x_raw = int32(csvread('../components/vilnius_oscillator/sim/test_out_x.csv'));
 y_raw = int32(csvread('../components/vilnius_oscillator/sim/test_out_y.csv'));
-z_raw = int32(csvread('../components/vilnius_oscillator/sim/test_out_z.csv'));
+%z_raw = int32(csvread('../components/vilnius_oscillator/sim/test_out_z.csv'));
 
 x_int = bitshift(x_raw, -16);
 y_int = bitshift(y_raw, -16);
-z_int = bitshift(z_raw, -16);
+%z_int = bitshift(z_raw, -16);
 
 x_frac = bitand(x_raw, 65535);
 y_frac = bitand(y_raw, 65535);
-z_frac = bitand(z_raw, 65535);
+%z_frac = bitand(z_raw, 65535);
 
 x_fpga = double(x_int) + double(x_frac) .* 2^(-16);
 y_fpga = double(y_int) + double(y_frac) .* 2^(-16);
-z_fpga = double(z_int) + double(z_frac) .* 2^(-16);
+%z_fpga = double(z_int) + double(z_frac) .* 2^(-16);
 
-plot3(x_fpga, y_fpga, z_fpga);
+plot(x_fpga, y_fpga);
+xlabel('{\itx}');
+ylabel('{\ity}');
+legend('MATLAB', 'Questa Simulation')
